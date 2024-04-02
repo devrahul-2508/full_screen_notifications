9ac06d7e674880121fb115fd53bfd444fcf63a64
devrahul-2508 <devrahul2508@gmail.com>
2024-04-02 11:23:37 +0530

    added lock

--- android/app/src/main/kotlin/com/example/full_screen_notification/NotificationActivity.kt
+++ android/app/src/main/kotlin/com/example/full_screen_notification/NotificationActivity.kt
@@ -1,18 +1,18 @@
 package com.example.full_screen_notification
 
+import android.app.Activity
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
 
 class NotificationActivity : FlutterActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,18 +27,56 @@ class NotificationActivity : FlutterActivity() {
         val button = findViewById<Button>(R.id.btn)
 
         button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish() // Finish the current activity so that it's not kept in the back stack
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+            if (keyguardManager.isDeviceLocked){
+                if (keyguardManager.isKeyguardSecure) {
+
+                    val intent = keyguardManager.createConfirmDeviceCredentialIntent(null, null);
+                    startActivityForResult(intent, REQUEST_PIN_OR_PASSWORD)
+
+
+                } else {
+                    // Device is not locked, start the activity directly
+                    navigateToMainActivity()
+                }
+            }
+            else{
+                navigateToMainActivity()
+            }
+
         }
 
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_PIN_OR_PASSWORD) {
+            if (resultCode == Activity.RESULT_OK) {
+                // User entered correct PIN or password, start the activity
+              navigateToMainActivity()
+            } else {
+                // User canceled or entered incorrect PIN or password
+                // Handle accordingly, for example, show a message or exit the app
+            }
+        }
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
-        // Start MainActivity when back button is pressed
+        navigateToMainActivity()
+    }
+    private fun navigateToMainActivity(){
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
-        finish() // Finish the current activity so that it's not kept in the back stack
+        finish()
     }
+
+    companion object {
+        const val REQUEST_PIN_OR_PASSWORD = 1001
+    }
+
+
+
 }
